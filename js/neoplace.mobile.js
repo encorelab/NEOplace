94a150790f855721b48f31c016ec16b186660684
Matt Zukowski <matt@zukowski.ca>
2012-05-01 18:00:32 -0400

    Initial skeleton

---
+++ js/neoplace.mobile.js
@@ -0,0 +1,20 @@
+/*jshint browser: true, devel: true */
+/*globals Sail, jQuery */
+var NEOplace = window.NEOplace || {};
+
+NEOplace.Mobile = (function() {
+    "use strict";
+    var self = {};
+
+    /** private function **/
+    var foo = function () {
+
+    };
+
+    /** public function **/
+    self.bar = function () {
+
+    };
+    
+    return self;
+})();
