2e178e039385b014a2cc004a7023e33aa95759c9
Armin Krauss <armin.krauss@utoronto.ca>
2012-05-28 15:47:35 -0400

    still trying ...

--- agents/scripts/create_classroom_results.rb
+++ agents/scripts/create_classroom_results.rb
@@ -10,16 +10,14 @@ require 'mongo'
 
 
 def create_results()
-  exits_phrase = {'$exists' => true}
-
   puts "Reading homework array"
   homework_problems = @mongo.collection(:aggregated_homework).find().to_a
   puts "Homework problems #{homework_problems.inspect}"
   puts "Homework size #{homework_problems.length}"
 
-  principle_events = @mongo.collection(:events).find("eventType" => "quorum_reached", "payload.principles" => exits_phrase.to_JSON).to_a
+  principle_events = @mongo.collection(:events).find("eventType" => "quorum_reached", "payload.principles" => {'$exists' => true}).to_a
   puts "Events with principles size #{principle_events.length}"
-  equation_events = @mongo.collection(:events).find("eventType" => "quorum_reached", "payload.equations" => exits_phrase.to_JSON).to_a
+  equation_events = @mongo.collection(:events).find("eventType" => "quorum_reached", "payload.equations" => {'$exists' => true}).to_a
   puts "Events with equations size #{equation_events.length}"
 end
 
