f1a6c5990b2aecdd873eca4bba20c59445c1d2b3
Matt Zukowski <matt@zukowski.ca>
2012-05-28 14:26:42 -0400

    added srtarter code for frontboard-2

---
+++ smartroom/frontboard-2/js/neoplace.frontboard2.js
@@ -0,0 +1,12 @@
+/*jshint browser: true, devel: true */
+/*globals Sail, jQuery, _, Rollcall, NEOplace */
+
+var NEOplace = window.NEOplace || {};
+
+NEOplace.FrontBoard2 = (function() {
+    var module = {};
+
+    
+
+    return module;
+})();
