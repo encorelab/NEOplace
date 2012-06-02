5e948f101e157c99931a12cf9ff71fef1678e28f
mikecules <michael.moncada@gmai.com>
2012-06-02 19:40:43 -0400

    Added timer functionality, animation queue of task achievements.

--- smartroom/frontwall-tracker/js/neoplace_FrontBoard.js
+++ smartroom/frontwall-tracker/js/neoplace_FrontBoard.js
@@ -1,6 +1,6 @@
+  // global variables....
   var players = {};
-  
-  //left top
+
   var playerLocationOrigins = [[0,0],[210,60],[560,60], [210,410], [560,410]];
   var playerLocationsAllocations = {};
   var previousPlayerAllocation = {};
@@ -14,6 +14,166 @@
   var maxNumberOfPlayersPerLocation = 3;
   var counter = 0;
   var taskAnimationStarted = false;
+  
+  var taskTimeBoundariesInMinutes = [
+                          [],
+                          [0.2,0.5,0.6],
+                          [1.5, 2.0, 2.0],
+                          [8.0, 13.0, 15.0],
+                          [10.0, 18.0, 20.0],
+                          [11.0, 18.0, 20.0]
+                          ];
+  
+  var statusCaptions = ['Welcome to NEOplace!',
+                        'Lots of time left!',
+                        'Time is running out!',
+                        'Better finish up!',
+                        'Time is up!'];
+  
+  var currentTask = null;
+  var currentTaskTickTimer = 0;
+  var tickInterval = 200;
+  
+  /// GLOBAL FUNCTIONS //////
+  function setCurrentTask(taskID) {
+    currentTaskTickTimer = 0;
+    currentTask = taskID;
+  }
+  
+  function frontboardTrackerProcessor(){
+    
+    if (currentTask) {
+      currentTaskTickTimer += tickInterval;
+      taskStatusAnimationProcessor();
+    }
+    
+    animateTaskCompletionAnimationProcessor();
+    
+    
+    setTimeout("frontboardTrackerProcessor()", tickInterval);
+  }
+  
+  
+  
+  function taskStatusAnimationProcessor() {
+    var ticksInSeconds = Math.floor(currentTaskTickTimer/1000);
+    var medianTaskTimeBoundaryInSeconds = Math.ceil(taskTimeBoundariesInMinutes[currentTask][0] * 60);
+    var upperTaskTimeBoundaryInSeconds = Math.ceil(taskTimeBoundariesInMinutes[currentTask][1] * 60);
+    var maxTaskTimeBoundaryInSeconds = Math.ceil(taskTimeBoundariesInMinutes[currentTask][2] * 60);
+    //alert(ticksInSeconds);
+    
+    if (ticksInSeconds >= medianTaskTimeBoundaryInSeconds && ticksInSeconds < upperTaskTimeBoundaryInSeconds) {
+      jQuery('#progress-bar').removeClass('good-time').removeClass('stop-time').addClass('caution-time');
+      jQuery('#progress-text').html(statusCaptions[2]);
+    }
+    else if (ticksInSeconds >= upperTaskTimeBoundaryInSeconds && ticksInSeconds < maxTaskTimeBoundaryInSeconds) {
+      jQuery('#progress-bar').removeClass('good-time').removeClass('caution-time').addClass('stop-time');
+      jQuery('#progress-text').html(statusCaptions[3]);
+    }
+    else if (ticksInSeconds >= maxTaskTimeBoundaryInSeconds) {
+      jQuery('#progress-bar').removeClass('good-time').removeClass('caution-time').removeClass('stop-time');
+      jQuery('#progress-text').html(statusCaptions[4]);
+    }
+    else {
+      jQuery('#progress-bar').removeClass('stop-time').removeClass('caution-time').addClass('good-time');
+      jQuery('#progress-text').html(statusCaptions[1]);
+    }
+  }
+  
+  function animateTaskCompletionAnimationProcessor() {
+    if (taskAnimationStarted) return;
+    
+  
+    var groupID = null;
+    var taskName = null;
+    var taskID = null;
+     
+    
+    // find the first group in the array and grab it
+    for (gID in taskRewardsAnimationQueue) {
+      if (taskRewardsAnimationQueue[gID] !== false) {
+        groupID = gID;
+        taskID = taskRewardsAnimationQueue[gID];
+        break;
+      }
+    }
+    // no animations in the queue so return...
+    if (groupID === null) return;
+    
+    
+    taskAnimationStarted = true;
+    taskRewardsAnimationQueue[groupID] = false;
+    
+    var backgroundModal = jQuery("#modal-background");
+    
+    if (jQuery(backgroundModal).length == 0) {
+      jQuery("body").append('<div id="modal-background" style="display: none"></div>');
+      backgroundModal = jQuery("#modal-background");
+    }
+    jQuery('#achievement-symbol').attr('src', 'images/icon_' + activities[taskID] + '_large.png');
+    jQuery('#achievement-symbol').show();
+    jQuery('#achievement-description').html('Group ' + groupNames[groupID] + " Completed " + activityNames[taskID] + "!!");
+    jQuery(backgroundModal).fadeIn(800, "linear", function() {
+        jQuery("#epic-task-completed-container").fadeIn('fast');
+        jQuery("#mario").css({top:"800px", left: "730px"}).show();
+        jQuery("#mario").animate({top:"-=370px", left: "-=240px"},1200, "easeInQuad", function(){ jQuery('#achievement-symbol').hide('explode',{pieces: 40}, 950); jQuery("#task-completed-container").effect('bounce', 500);}).animate({top:"+=370px", left: "-=240px"}, 700, "easeOutQuad", function() {
+          
+          jQuery(this).fadeOut('fast', "linear", function(){
+              jQuery("#epic-task-completed-container").hide();
+              jQuery('#achievement-symbol').show();
+              jQuery(backgroundModal).fadeOut('fast', "linear", function() {
+                  taskAnimationStarted = false;
+                  rewardLocalUsersWithTaskTrophy(groupID, taskID);
+              });
+              })});
+      });
+  
+  }
+  
+  function rewardLocalUsersWithTaskTrophy(theLocale, taskID) {
+    for (playerID in players) {
+      
+      if (players[playerID]["location"] == theLocale) { 
+        addItem(playerID, taskID);
+      }
+    }
+  }
+  
+   function addItem(playerID, taskID) {
+    
+    if (getLength(players[playerID]["activityHistory"]) == 0) {
+      players[playerID]["activityHistory"] = {};
+      //alert('yo!');
+    }
+    
+    
+    players[playerID]["activityHistory"][taskID] = taskID;
+    //alert( getLength(players[playerID]["activityHistory"]));
+    var distanceLeft = getLength(players[playerID]["activityHistory"])*10;
+    var trophyImgID = "trophy_" + playerID;
+    
+    jQuery('#' + playerID).find('.trophies').append('<img id="' + trophyImgID + '" alt="Step ' + taskID + ' Completed!" style="position:relative; left: ' + distanceLeft + 'px;" src="images/icon_' + activities[taskID] + '.png" />');
+    jQuery('#' + playerID).find('.user-name').animate({top: '20px'});
+    
+    jQuery('#' + trophyImgID).show('slide', {direction: 'left'}, 'slow');
+   
+  }
+  
+  
+  function getLength(o) {
+    var length = 0;
+
+    for (var i in o) {
+      
+        length++;
+    }
+    return length;
+  }
+  
+  
+  function animateTaskCompletion(groupID, taskID) {
+    taskRewardsAnimationQueue[groupID] = taskID;
+  }
   
 jQuery(function() {
  // Handler for .ready() called.
@@ -37,59 +197,6 @@ jQuery(function() {
   };
   
   
-  var getLength = function(o) {
-    var length = 0;
-
-    for (var i in o) {
-      
-        length++;
-    }
-    return length;
-  }
-  
-  
-  var animateTaskCompletion = function(groupID, taskName, taskID) {
-    
-    if (taskAnimationStarted) return;
-    taskAnimationStarted = true;
-    
-    var backgroundModal = jQuery("#modal-background");
-    if (jQuery(backgroundModal).length == 0) {
-      jQuery("body").append('<div id="modal-background" style="display: none"></div>');
-      backgroundModal = jQuery("#modal-background");
-    }
-      jQuery('#achievement-description').html('Group ' + groupID + " Completed " + taskName + "!!");
-      jQuery(backgroundModal).fadeIn(800, "linear", function() {
-          
-          jQuery("#mario").animate({top:"-=386px", left: "-=240px"},800, "easeInQuad", function(){jQuery("#task-completed-container").effect('bounce', 400);}).animate({top:"+=386px", left: "-=240px"}, 800, "easeOutQuad", function() {
-            
-            jQuery(this).fadeOut('fast', "linear", function(){jQuery("#epic-task-completed-container").hide(); jQuery("#epic-task-completed-container img").show();  jQuery(backgroundModal).fadeOut('fast', "linear", function() { taskAnimationStarted = false; });})});
-        });
-      
-    
-    
-  };
-  
-  var addItem = function(playerID, taskID) {
-    
-    animateTaskCompletion(groupNames[1], activityNames[taskID], taskID);
-    if (getLength(players[playerID]["activityHistory"]) == 0) {
-      players[playerID]["activityHistory"] = {};
-      //alert('yo!');
-    }
-    
-    
-    players[playerID]["activityHistory"]["" + taskID] = taskID;
-   //alert( getLength(players[playerID]["activityHistory"]));
-    var distanceLeft = getLength(players[playerID]["activityHistory"])*10 + 22;
-   
-    
-    jQuery('#' + playerID).find('.trophies').append('<img alt="Step ' + taskID + ' Completed!" style="position:relative; left: ' + distanceLeft + 'px;" src="images/icon_' + activities[taskID] + '.png" />');
-    
-   
-  };
-
-  
   var movePlayer = function(playerID, newLocation) {
     
     if (newLocation == previousPlayerAllocation[playerID]) return;
@@ -100,19 +207,6 @@ jQuery(function() {
     
     jQuery('#' + playerID).animate({top:  playerLocationOrigins[newLocation][1] + (  playerLocationsAllocations[newLocation] * 75)+ "px", left: playerLocationOrigins[newLocation][0] +"px"}, 1000, function(){
 
-     
-      
-      /*jQuery('#loc-' + playerID).remove();
-      
-      if (newLocation % 2 == 1) {
-        jQuery('#' + playerID).find("div.triangle-right").removeClass('left').addClass('right');
-      }
-      else {
-        jQuery('#' + playerID).find("div.triangle-right").removeClass('right').addClass('left');
-      }
-      
-      jQuery("#location-" + newLocation).find("ul.location-player").append('<li id="loc-' + playerID + '">' + players[playerID]["userName"] + '</li>').fadeIn('fast');
-      */
       });
    
     if (previousPlayerAllocation[playerID] >= 0) {
@@ -133,11 +227,12 @@ jQuery(function() {
   };
   
   var spawnPlayers = function() {
-    var playerLength = 9;
+    var playerLength = 11;
     
     for (var i = 0; i <= numberOfLocations; i++) {
      
       playerLocationsAllocations[i] = 0;
+      taskRewardsAnimationQueue[i] = false;
     }
   
     for (var i = 0; i < playerLength; i++) {
@@ -160,43 +255,48 @@ jQuery(function() {
       players[playerID] = playerInfo;
       previousPlayerAllocation[playerID] = -1;
       
-      var timeout = Math.floor(Math.random() * 4);
-      var cls = '';
-      if (timeout == 0){
-        cls = 'blue';
+      var randomColour = Math.floor(Math.random() * 5);
+      var avatarColour = '';
+      
+      if (randomColour == 0){
+        avatarColour = 'blue';
       }
-      else if (timeout == 1) {
-        cls = 'green';
+      else if (randomColour == 1) {
+        avatarColour = 'green';
       }
-      else if (timeout == 2) {
-         cls = 'purple';
+      else if (randomColour == 2) {
+         avatarColour = 'purple';
         
       }
-      else if (timeout == 3) {
-         cls = 'yellow';
+      else if (randomColour == 3) {
+         avatarColour = 'yellow';
       }
       else {
-         cls = 'orange';
+         avatarColour = 'orange';
       }
       
-      jQuery("#locations").append('<div class="user" id="' + playerID +  '"><img alt="' + playerID + '" title="' + playerID + '" src="images/avatar_' + cls + '.png"  width="40" /><div class="triangle-right left ' + cls + '"><div class="trophies"></div><div class="user-name">' + playerID +'</div></div></div>');
+      jQuery("#locations").append('<div class="user" id="' + playerID +  '"><img alt="' + playerID + '" title="' + playerID + '" src="images/avatar_' + avatarColour + '.png"  width="40" /><div class="triangle-right left"><div class="trophies"></div><div class="user-name">' + playerID +'</div></div></div>');
       movePlayer(playerID, playerInfo["location"]);
       
-       addItem(playerID, 3);
-       addItem(playerID, 2);
+       
+       /*addItem(playerID, 2);
        addItem(playerID, 6);
        addItem(playerID, 4);
        addItem(playerID, 5);
-       addItem(playerID, 1);
+       addItem(playerID, 1);*/
     
     }
     
     
-    
-    
   };
-  
-  spawnPlayers();
  
-  jQuery(".user").click(function(){ movePlayer(jQuery(this).attr('id'), Math.floor(Math.random() * numberOfLocations) + 1); });
+  spawnPlayers();
+  setCurrentTask(1);
+  setTimeout("frontboardTrackerProcessor()", tickInterval);
+  
+  animateTaskCompletion(1, 1);
+ 
+  animateTaskCompletion(3, 6);
+  animateTaskCompletion(2, 3);
+  jQuery(".user").click(function(){  animateTaskCompletion(2, 1); movePlayer(jQuery(this).attr('id'), Math.floor(Math.random() * numberOfLocations) + 1); });
 });
