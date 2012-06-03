d0a9b1aecc89994126c01103bae2a238eb62957a
Armin Krauss <armin.krauss@utoronto.ca>
2012-06-03 17:25:26 -0400

    moved file into scripts

---
+++ agents/scripts/clear_mongo_db.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+require 'mongo'
+
+ARGV.each do|a|
+  puts "Argument: #{a}"
+end
+
+@mongo = Mongo::Connection.new.db(ARGV[0])
+#@mongo = Mongo::Connection.new.db('neo-cd')
+
+
+def clear_data()
+  puts "Clearing events"
+  @mongo.collection(:events).remove()
+  puts "Clearing observations"
+  @mongo.collection(:observations).remove()
+  puts "Clearing states"
+  @mongo.collection(:states).remove()
+  puts "Clearing problem_assignments"
+  @mongo.collection(:problem_assignments).remove()
+  puts "Clearing active_users"
+  @mongo.collection(:active_users).remove()
+end
+
+clear_data()
