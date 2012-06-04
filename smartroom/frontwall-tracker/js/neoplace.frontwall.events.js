2a8f97e0b8174f6fe207c42005198031c90606f7
mikecules <michael.moncada@gmai.com>
2012-06-03 20:50:07 -0400

    fixed message lookup of the location for events that dictate the end of an activity

--- smartroom/frontwall-tracker/js/neoplace.frontwall.events.js
+++ smartroom/frontwall-tracker/js/neoplace.frontwall.events.js
@@ -91,23 +91,23 @@
             }
        },
        videowall_principles_commit: function(sev) {
-            var locale = sev.payload.origin.toUpperCase(); //A,B,C,D;
+            var locale = sev.payload.videowall.toUpperCase(); //A,B,C,D;
             app.animateTaskCompletion(localeMapper[locale], 2);
        },
        videowall_problems_commit: function(sev) {
-            var locale = sev.payload.origin.toUpperCase(); //A,B,C,D;
+            var locale = sev.payload.videowall.toUpperCase(); //A,B,C,D;
             app.animateTaskCompletion(localeMapper[locale], 3);
        },
        videowall_equations_commit: function(sev) {
-            var locale = sev.payload.origin.toUpperCase(); //A,B,C,D;
+            var locale = sev.payload.videowall.toUpperCase(); //A,B,C,D;
             app.animateTaskCompletion(localeMapper[locale], 4);
        },
        videowall_assumptions_variables_commit: function(sev) {
-            var locale = sev.payload.origin.toUpperCase(); //A,B,C,D;
+            var locale = sev.payload.videowall.toUpperCase(); //A,B,C,D;
             app.animateTaskCompletion(localeMapper[locale], 5);
        },
        video_answer_complete: function(sev) {
-            var locale = sev.payload.origin.toUpperCase(); //A,B,C,D;
+            var locale = sev.payload.location.toUpperCase(); //A,B,C,D;
             app.animateTaskCompletion(localeMapper[locale], 6);
        }
     };
